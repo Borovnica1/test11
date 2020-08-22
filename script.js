ed9f90b3f2771c863789198d5987e2592182036b
Borovnica1 <45723338+Borovnica1@users.noreply.github.com>
2020-08-22 22:17:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var gameLogic = (function() {
   // function constructor , n broj igraca sa inputa i for loopa koja pravi igrace
-  var Player = function(id, name, budget, mapSpot) {
+  var Player = function(id, name, char, budget, mapSpot) {
     this.id = id;
     this.name = name;
+    this.char = char;
     this.budget = budget;
     this.mapSpot = mapSpot;
   }
@@ -63,10 +64,14 @@ var UIController = (function() {
 
     showPlayerCreate: function(playerNumber) {
 
-      html = '<div class="map__modal" style="width: 50%; position: absolute; top: 50%; left: 50%; background-color: pink; transform: translate(-50%, -50%); padding: 2rem;">' 
+      html = '<div class="map__modal" style="width: 50%; position: absolute; top: 50%; left: 50%; background-color: pink; transform: translate(-50%, -50%); padding: 1.6rem 2rem;">' 
       + '<h1>' + 'Player ' + playerNumber + '</h1>'
-      + 'Name: <input type="text" class="player__name">'
-      + 'chars'
+      + '<h2 style="margin-top: .4rem">' + 'Name:' + '</h2>' + '<input type="text" class="player__name" required style="display:block;">'
+      + '<h2 style="margin-top: .4rem">' + 'Choose a character:' + '</h2>'
+      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&phone;' + '</span>' + '</div>'
+      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&#9877;' + '</span>' + '</div>'
+      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&#10086;' + '</span>' + '</div>'
+      + '<button class="map__done" style="display:flex;margin:auto;padding:.3rem .6rem;margin-top:.4rem">' + 'Done' + '</button>'
       + '</div>';
 
       mapContainer.insertAdjacentHTML('beforeend', html);
@@ -102,7 +107,34 @@ var controller = (function(game, UICtrl) {
 
     });
   }
-  
+  var playerIsCreated = false;
+  var name = 'lol';
+  var char = 'xoxo';
+  function updateEventListener2() {
+    var playerL = document.querySelectorAll('.map__box2');
+    console.log(playerL);
+    var lastItem = document.querySelector('.map__box2');
+    playerL.forEach(item => {
+      
+      item.addEventListener('click', exe => {
+        lastItem.style.border = "1px solid #000";
+        item.style.border = "1px solid red";
+        lastItem = item;
+        char = item.firstChild.innerHTML;
+      })
+    })    
+    
+
+    document.querySelector('.map__done').addEventListener('click', function(){
+      name = document.querySelector('.player__name').value;
+      console.log(name, char, playerIsCreated);
+      if (name.length > 0 && char.length > 0) {
+        playerIsCreated = true;
+        document.querySelector('.map__modal').parentNode.removeChild(document.querySelector('.map__modal'));
+      }
+    });
+    
+  }
 
   async function createGame() {
     UICtrl.showPlayerPanel();
@@ -112,13 +144,15 @@ var controller = (function(game, UICtrl) {
     while (!numberIsPicked) {
       await new Promise(r => setTimeout(r, 0100));
     }
-    
+
     while (broj > 0) {
       UICtrl.showPlayerCreate(broj);
+      updateEventListener2();
+
       while (!playerIsCreated) {
-        await new Promise(r => setTimeout(r, 1000));
+        await new Promise(r => setTimeout(r, 0100));
       }
-      
+      playerIsCreated = false;
       broj--;
     }
     
