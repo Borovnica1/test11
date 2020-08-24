add9ceb7bb9bd284d047182f897a6d7d32d55700
Borovnica1 <petkovic.czv1@gmail.com>
2020-08-24 16:50:56 +0200

    createGame function created

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var gameLogic = (function() {
   var Player1 = new Player(1, 'John', 22);
   console.log(Player1.name);
 
+  var players = [];
 
   /* ovde nastavi!!
   var makePlayers = function(numberOfPlayers) {
@@ -23,7 +24,15 @@ var gameLogic = (function() {
 
 
   return {
-    
+    addPlayer: function(id, name, char) {
+      var newPlayer = new Player(id, name, char, 1500, 1);
+      players.unshift(newPlayer);
+      console.log(players);
+    },
+
+    getPlayers: function() {
+      return players;
+    },
 
   }
 
@@ -63,7 +72,6 @@ var UIController = (function() {
     },
 
     showPlayerCreate: function(playerNumber) {
-
       html = '<div class="map__modal" style="width: 50%; position: absolute; top: 50%; left: 50%; background-color: pink; transform: translate(-50%, -50%); padding: 1.6rem 2rem;">' 
       + '<h1>' + 'Player ' + playerNumber + '</h1>'
       + '<h2 style="margin-top: .4rem">' + 'Name:' + '</h2>' + '<input type="text" class="player__name" required style="display:block;">'
@@ -75,6 +83,23 @@ var UIController = (function() {
       + '</div>';
 
       mapContainer.insertAdjacentHTML('beforeend', html);
+    },
+
+    showPlayerDashboard: function(players) {
+      for (var i = 0; i < players.length; i++) {
+        console.log(players[i].name);
+        html = '<div class="stats__player'+players[i].id+'" style="height=300px;background-color:lightblue;display:flex;margin-bottom: .4rem">'
+        + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display:flex;justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + players[i].char + '</span>' + '</div>'
+        + '<div style="margin-left: .3rem">'
+        + '<h2>' + players[i].name + '</h2>'
+        + '<h3 style="margin-left: .5rem;margin-top:.2rem">' + players[i].budget + '</h3>'
+        + '</div>'
+        + '</div>';
+
+        document.querySelector('.stats').insertAdjacentHTML('beforeend', html);
+
+        // u istoj for petlji dodaj igrace i na kartu!
+      }
     }
   }
 })();
@@ -100,38 +125,37 @@ var controller = (function(game, UICtrl) {
     playersNumber.forEach(item => {
 
       item.addEventListener('click', exe => {
-        broj = item.getAttribute('data-id'); 
+        broj = parseInt(item.getAttribute('data-id')); 
         numberIsPicked = true;
         document.querySelector('.map__modal').parentNode.removeChild(document.querySelector('.map__modal'));
       });
-
     });
   }
   var playerIsCreated = false;
-  var name = 'lol';
-  var char = 'xoxo';
+  var name,
+      char;
   function updateEventListener2() {
+    name = '',
+    char = '';
     var playerL = document.querySelectorAll('.map__box2');
-    console.log(playerL);
     var lastItem = document.querySelector('.map__box2');
     playerL.forEach(item => {
-      
       item.addEventListener('click', exe => {
         lastItem.style.border = "1px solid #000";
         item.style.border = "1px solid red";
         lastItem = item;
         char = item.firstChild.innerHTML;
       })
-    })    
+    })
     
 
     document.querySelector('.map__done').addEventListener('click', function(){
       name = document.querySelector('.player__name').value;
-      console.log(name, char, playerIsCreated);
       if (name.length > 0 && char.length > 0) {
         playerIsCreated = true;
         document.querySelector('.map__modal').parentNode.removeChild(document.querySelector('.map__modal'));
       }
+      console.log(name, char, playerIsCreated);
     });
     
   }
@@ -152,10 +176,14 @@ var controller = (function(game, UICtrl) {
       while (!playerIsCreated) {
         await new Promise(r => setTimeout(r, 0100));
       }
+      game.addPlayer(broj, name, char);
+
       playerIsCreated = false;
       broj--;
     }
-    
+    var playersArr = game.getPlayers();
+    UICtrl.showPlayerDashboard(playersArr);
+
 
   };
 
@@ -166,4 +194,5 @@ var controller = (function(game, UICtrl) {
   // event listeners should be at the end cuz only then i can assign functions to var!!
   setupEventListeners();
 })(gameLogic, UIController);
-// test git
+
+// make video cut extension and display on the board random cuts
