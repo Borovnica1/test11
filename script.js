3c98776bef0ab8785d26ba745d1c0daf8dbb14c7
Borovnica1 <petkovic.czv1@gmail.com>
2020-08-26 17:39:35 +0200

    sorting Players done and createGame hopefully done

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var gameLogic = (function() {
   // function constructor
-  var Player = function(id, name, char, budget, mapSpot) {
+  var Player = function(id, name, char, budget, mapSpot, rolledNumber) {
     this.id = id;
     this.name = name;
     this.char = char;
     this.budget = budget;
     this.mapSpot = mapSpot;
+    this.rolledNumber = rolledNumber;
   }
 
   Player.prototype.movePlayer = function() {
@@ -21,16 +22,24 @@ var gameLogic = (function() {
 
   return {
     addPlayer: function(id, name, char) {
-      var newPlayer = new Player(id, name, char, 1500, 31);
+      var newPlayer = new Player(id, name, char, 1500, 31, 0);
       players.unshift(newPlayer);
       console.log(players);
     },
 
-    addDiceRoll: function() {
+    addDiceRoll: function(player) {
       var dice1 = Math.floor(Math.random() * 6) + 1;
       var dice2 = Math.floor(Math.random() * 6) + 1;
       dices = [dice1, dice2];
-      diceRolls.push(dices);
+      diceRolls.unshift(dices);
+
+      // add to Player property dice number!!
+      player.rolledNumber = dice1 + dice2;
+    },
+
+    sortPlayers: function(playersArr) {
+      playersArr.sort((a, b) => b.rolledNumber - a.rolledNumber);
+      diceRolls.sort((a, b) => (b[0] + b[1]) - (a[0] + a[1]));
     },
 
     getPlayers: function() {
@@ -39,6 +48,10 @@ var gameLogic = (function() {
 
     getDices: function() {
       return dices;
+    },
+
+    getRolledDices: function() {
+      return diceRolls;
     }
 
   }
@@ -81,25 +94,30 @@ var UIController = (function() {
     showPlayerCreate: function(playerNumber) {
       html = '<div class="map__modal" style="width: 50%; position: absolute; top: 50%; left: 50%; background-color: pink; transform: translate(-50%, -50%); padding: 1.6rem 2rem;">' 
       + '<h1>' + 'Player ' + playerNumber + '</h1>'
-      + '<h2 style="margin-top: .4rem">' + 'Name:' + '</h2>' + '<input type="text" class="player__name" required style="display:block;">'
+      + '<h2 style="margin-top: .4rem">' + 'Name:' + '</h2>' + '<input type="text" class="player__name" required maxlength="20" style="display:block;height:1.5rem;outline:none;border:none;">'
       + '<h2 style="margin-top: .4rem">' + 'Choose a character:' + '</h2>'
-      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&phone;' + '</span>' + '</div>'
-      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&#9877;' + '</span>' + '</div>'
-      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&#10086;' + '</span>' + '</div>'
-      + '<button class="map__done" style="display:flex;margin:auto;padding:.3rem .6rem;margin-top:.4rem">' + 'Done' + '</button>'
+      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  40px; height: 40px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&phone;' + '</span>' + '</div>'
+      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  40px; height: 40px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&#9877;' + '</span>' + '</div>'
+      + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  40px; height: 40px; border-radius:50%; overflow:hidden; display: inline-flex; justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + '&#10086;' + '</span>' + '</div>'
+      + '<button class="map__done" style="display:flex;margin:auto;padding:.6rem .9rem;margin-top:.4rem">' + 'Done' + '</button>'
       + '</div>';
 
       mapContainer.insertAdjacentHTML('beforeend', html);
     },
 
     showPlayerDashboard: function(players) {
+      // Clears the previous created dashboard of players!!
+      document.querySelector('.stats').innerHTML = '';
+      document.querySelector('[data-id="'+players[0].mapSpot+'"]').innerHTML = '';
+
       for (var i = 0; i < players.length; i++) {
         html = '<div class="stats__player'+players[i].id+'" style="height=300px;background-color:lightblue;display:flex;margin-bottom: .4rem">'
-        + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  30px; height: 30px; border-radius:50%; overflow:hidden; display:flex;justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + players[i].char + '</span>' + '</div>'
+        + '<div class="map__box2" style="cursor:pointer; border: 1px solid #000; width:  40px; height: 40px; border-radius:50%; overflow:hidden; display:flex;justify-content: center; margin-left: .5rem;">' + '<span class="map__char" style="display:flex; align-items: center; font-size: 22px;">'  + players[i].char + '</span>' + '</div>'
         + '<div style="margin-left: .3rem">'
-        + '<h2>' + players[i].name + '</h2>'
-        + '<h3 style="margin-left: .5rem;margin-top:.2rem">' + players[i].budget + '</h3>'
+        + '<h1>' + players[i].name + '</h1>'
+        + '<h2 style="margin-left: .8rem;margin-top:.2rem;color:darkgreen">' + players[i].budget + '</h2>'
         + '</div>'
+        + '<div class="stats__rolled'+i+'" style="margin-left:auto;display:flex;justify-content:center;align-items:center;">' + '' + '</div>'
         + '</div>';
         document.querySelector('.stats').insertAdjacentHTML('beforeend', html);
 
@@ -111,24 +129,40 @@ var UIController = (function() {
       }
     },
 
-    showRollDice: function() {
+    showRollDice: function(name) {
       var rollDiceBtn = document.querySelector('.rollDice');
+      var playerNumber = document.querySelector('.playerNumber');
+      playerNumber.innerHTML = '<h1> Player: ' + name + '</h1>';
       mapContainer.insertAdjacentElement('beforeend', rollDiceBtn);
       rollDiceBtn.style.display = 'block';
+      playerNumber.style.display = 'block';
     },
 
-    showDices: function(dices) {
-      htmlDice1 = '<img src="dices/dice-'+dices[0]+'.png">';
-      htmlDice2 = '<img src="dices/dice-'+dices[1]+'.png">';
+    showDices: function(dices, index) {
+      htmlDice1 = '<img src="dices/dice-'+dices[0]+'.png" style="width: 100px; height:100px;border-radius:10px">';
+      htmlDice2 = '<img src="dices/dice-'+dices[1]+'.png" style="width: 100px; height:100px;border-radius:10px">';
       document.querySelector('.rollDice1').insertAdjacentHTML('beforeend', htmlDice1);
       document.querySelector('.rollDice2').insertAdjacentHTML('beforeend', htmlDice2);
       document.querySelector('.rollDice1').style.display = 'block';
-      document.querySelector('.rollDice1').style.display = 'block';
+      document.querySelector('.rollDice2').style.display = 'block';
+      document.querySelector('.playerNumber').innerHTML = '<h2>' + 'Rolled: ' + (dices[0] + dices[1]) + '</h2>';
+
+      // Now changing size of dices
+      htmlDice1 = '<img src="dices/dice-'+dices[0]+'.png" style="width: 25px; height:25px; border-radius:5px;margin: 0 .3rem">';
+      htmlDice2 = '<img src="dices/dice-'+dices[1]+'.png" style="width: 25px; height:25px;border-radius:5px;margin-right:.3rem">';
+      document.querySelector('.stats__rolled'+index).innerHTML = '<h2>Rolled: ' + (dices[0] + dices[1]) + '</h2>' + htmlDice1 + htmlDice2;
+    },
+
+    showDicesNextToPlayerName: function(dices, sortedPlayer) {
+      htmlDice1 = '<img src="dices/dice-'+dices[0]+'.png" style="width: 25px; height:25px; border-radius:5px;margin: 0 .3rem">';
+      htmlDice2 = '<img src="dices/dice-'+dices[1]+'.png" style="width: 25px; height:25px;border-radius:5px;margin-right:.3rem">';
+      document.querySelector('.stats__rolled'+sortedPlayer).innerHTML = '<h2>Rolled: ' + (dices[0] + dices[1]) + '</h2>' + htmlDice1 + htmlDice2;
     },
 
     hideDices: function() {
-      document.querySelector('.rollDice1').style.display = 'none';
-      document.querySelector('.rollDice1').style.display = 'none';
+      document.querySelector('.rollDice1').innerHTML = '';
+      document.querySelector('.rollDice2').innerHTML = '';
+      document.querySelector('.playerNumber').innerHTML = '';
     }
 
   }
@@ -225,19 +259,28 @@ var controller = (function(game, UICtrl) {
 
     // Roll dice for play order!!
     while (broj2 > 0) {
-      UICtrl.showRollDice();
+      UICtrl.showRollDice(playersArr[broj2 - 1].name);
       updateEventListener3();
       while (!diceClicked) {
         await new Promise(r => setTimeout(r, 0100));
       }
-      game.addDiceRoll();
+      game.addDiceRoll(playersArr[broj2 - 1]);
       var dices = game.getDices();
-      UICtrl.showDices(dices);
-      await new Promise(r => setTimeout(r, 1000));
+      UICtrl.showDices(dices, broj2 - 1);
+      await new Promise(r => setTimeout(r, 0500));
       UICtrl.hideDices();
       diceClicked = false;
       broj2--;
+    };
+    // Now sort players by rolledNumber!!
+    game.sortPlayers(playersArr);
+    UICtrl.showPlayerDashboard(playersArr);
+    var rolledDices = game.getRolledDices();
+    console.log(rolledDices, playersArr);
+    for (var i = 0; i < rolledDices.length; i++) {
+      UICtrl.showDicesNextToPlayerName(rolledDices[i], i);
     }
+    
     
   };
 
@@ -250,3 +293,5 @@ var controller = (function(game, UICtrl) {
 })(gameLogic, UIController);
 
 // make video cut extension and display on the board random cuts
+
+// svaka stranka druga boja i special effect???!?!?!?!?
